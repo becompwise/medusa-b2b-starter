0722cf4285caa439d153c935a31e53e4432b1178
becompwise <becompwise@gmail.com>
2025-04-29 11:31:29 -0700

    env files

--- backend/src/api/middlewares.ts
+++ backend/src/api/middlewares.ts
@@ -2,21 +2,21 @@ import {
   MedusaNextFunction,
   MedusaRequest,
   MedusaResponse,
-} from "@medusajs/framework";
-import { defineMiddlewares } from "@medusajs/medusa";
-import { adminMiddlewares } from "./admin/middlewares";
-import { storeMiddlewares } from "./store/middlewares";
+} from '@medusajs/framework';
+import { defineMiddlewares } from '@medusajs/medusa';
+import { adminMiddlewares } from './admin/middlewares';
+import { storeMiddlewares } from './store/middlewares';
 
 export default defineMiddlewares({
   routes: [
     ...adminMiddlewares,
     ...storeMiddlewares,
     {
-      matcher: "/store/customers/me",
-      method: "GET",
+      matcher: '/store/customers/me',
+      method: 'GET',
       middlewares: [
         (req: MedusaRequest, res: MedusaResponse, next: MedusaNextFunction) => {
-          req.allowed = ["employee"];
+          req.allowed = ['employee'];
           next();
         },
       ],
---
+++ backend/src/api/store/customers/middelwares.ts
@@ -0,0 +1,17 @@
+import {
+  authenticate,
+  validateAndTransformBody,
+  validateAndTransformQuery,
+} from '@medusajs/framework';
+import { MiddlewaresConfig } from '@medusajs/medusa';
+export const config: MiddlewaresConfig = {
+  routes: [
+    {
+      method: ['GET'], // HTTP verb(s)
+      matcher: '/store/customers/:id/cart', // the exact path
+      middlewares: [
+        authenticate('customer', ['bearer', 'session']), // require a logged-in customer
+      ],
+    },
+  ],
+};
